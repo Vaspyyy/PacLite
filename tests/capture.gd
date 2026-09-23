584e50d5e2d8947ac2111bc887a3ce746118fbe8
Ransom <lolbautz2@gmail.com>
2026-09-23 20:40:12 +0200

    [draft-release] Capture gameplay and package first milestone

---
+++ tests/capture.gd
@@ -0,0 +1,21 @@
+extends SceneTree
+
+const MainScene = preload("res://scenes/main.tscn")
+
+func _initialize() -> void:
+	call_deferred("_capture")
+
+func _capture() -> void:
+	var scene = MainScene.instantiate()
+	root.add_child(scene)
+	scene._new_run()
+	for i in 12:
+		await process_frame
+	var image := root.get_texture().get_image()
+	var result := image.save_png("res://build/first-floor.png")
+	if result != OK:
+		push_error("Could not save gameplay screenshot")
+		quit(1)
+		return
+	print("Saved first-floor.png")
+	quit(0)
